404279fd5c0b9158dae2ae91721858c3e25cd91c
Oli <ojgreensemail@gmail.com>
2025-02-21 13:33:39 +0000

    Cleaning up

--- lua/bsky-nvim/init.lua
+++ lua/bsky-nvim/init.lua
@@ -3,11 +3,6 @@ local BS = {}
 local config = require("bsky-nvim.config")
 local post = require('bsky-nvim.post')
 
-function BS.open_posting_window()
-  --- TODO!: Open a window that a user can
-  --- create posts in and send on <CR>
-end
-
 function BS.setup(opts)
   opts = opts or vim.notify("Configuration not setup")
   if opts then
